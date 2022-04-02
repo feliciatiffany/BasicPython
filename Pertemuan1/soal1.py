da8535312db57e277000acc7b49b926c16783f94
feliciatiffany <feliciatiffany@gmail.com>
2022-04-02 11:45:23 +0700

    Pertemuan1/soal1.py

---
+++ Pertemuan1/soal1.py
@@ -0,0 +1,5 @@
+nama=input("Masukkan nama : ")
+umur=input("Masukkan umur : ")
+tinggi=input("Masukkan tinggi badan : ")
+
+print ("Nama saya {}, umur  {} tahun dan tinggi saya {}".format (nama,umur,tinggi))
---
+++ Pertemuan1/soal2.py
@@ -0,0 +1,4 @@
+jari = float(input("masukkan jari-jari = "))
+phi = float(22.0/7)
+luas= phi*jari*jari
+print ("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm""\u00b2".format(jari,luas))
