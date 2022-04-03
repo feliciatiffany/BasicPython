a953a7d687a1ecf53fec4ff313edf7f1190fc3f7
feliciatiffany <feliciatiffany@gmail.com>
2022-04-03 12:54:01 +0700

    .

---
+++ assignment#2/BasicPython-Tugas2.py
@@ -0,0 +1,48 @@
+def daftarkontak(kontak):
+    if len(kontak) == 0:
+        print ("Belum ada kontak didaftarkan")
+        print (" ")
+        return
+    for i in range(len(kontak)):
+        mydict=kontak[str(i+1)]
+        print ("Nama : ", mydict["nama"])
+        print ("No Telepon : ", mydict["no"])
+        print (" ")
+        
+def tambahkontak(kontak):
+        mydict = {
+            "nama": "lala",
+            "no": "000",
+        }
+        print ("Tambah Kontak ")
+        print (" ")
+        mydict["nama"] = input("Nama : ")
+        mydict["no"] = input("No Telepon: ")
+        kontak[str(len(kontak)+1)] = mydict
+        return kontak
+
+kontak={}
+while True:
+    print ("Selamat Datang\n")
+    print ("--- Menu ---")
+    print ("1. Daftar Kontak")
+    print ("2. Tambah Kontak")
+    print ("3. Keluar")
+    print (" ")
+    i=input("Pilih Menu : ")
+    print (" ")
+    
+    if int(i) == 1:
+        daftarkontak(kontak)
+    elif int(i)==2:
+        kontak=tambahkontak(kontak)
+        print ("Kontak berhasil ditambahkan")
+        print (" ")
+    elif int(i)==3:
+        print ("Program selesai, sampai jumpa!")
+        break
+    else:
+        print ("Menu tidak tersedia")
+        print (" ")
+        
+        
